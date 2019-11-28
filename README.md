18a8f3f827522b1d83b6064bd0932d0a2e174fa1
Otávio Fernandes <otaviof@gmail.com>
2019-11-28 20:44:43 +0100

    WIP.

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # refined-bot
 
 One stop bot for refining your Agile Process, with easy and automated poker style story onboarding.
+
+## Contributing
+
+### Getting Started
+
+```bash
+pip3 install tox
+tox
+source .tox/python/bin/activate
+```
--- refined-bot/nlp/nlp.py
+++ refined-bot/nlp/nlp.py
@@ -1,29 +1,37 @@
 import re
 
 import spacy
+from spacy.lang.en import English
 from spacy.matcher import Matcher
+from spacy.tokens import Token
 
 from .intent import Intent
 
 
 class NLP():
-    """ Represents the NLP steps taken to extact "Intent". """
+    """
+        Represents the NLP steps taken to extact "Intent".
+    """
 
-    nlp = None
-    matcher = None
+    nlp: English = None
+    matcher: Matcher = None
 
     # regular expression to find an ticket
     ticketIdRe = r'[A-Z]{3,6}\-[0-9]{1,4}'
     ticketIdPattern = [{'TAG': {'REGEX': ticketIdRe}}]
 
     def __init__(self):
-        """ Instantiate class by executing spacy boilerplates. """
+        """
+            Instantiate class by executing spacy boilerplates.
+        """
         self.nlp = spacy.load('en_core_web_sm')
         self.matcher = Matcher(self.nlp.vocab, validate=True)
         self.matcher.add('TicketID', None, self.ticketIdPattern)
 
-    def __findTicketID(self, doc):
-        """ Using regexp will try to find a ticket ID. """
+    def __findTicketID(self, doc: Token.doc) -> str:
+        """
+            Using regexp will try to find a ticket ID.
+        """
         for match in re.finditer(self.ticketIdRe, doc.text):
             start, end = match.span()
             span = doc.char_span(start, end)
@@ -31,8 +39,9 @@ class NLP():
                 return span.text
         return None
 
-    def __findLemma(self, doc):
-        """ Will try to identify the characteristics of the message in order to define what's the
+    def __findLemma(self, doc: Token.doc) -> str:
+        """
+            Will try to identify the characteristics of the message in order to define what's the
             action conveyed. It should return as soon as the "lemma" is defined.
         """
         for token in doc:
@@ -40,8 +49,10 @@ class NLP():
                 return token.lemma_
         return None
 
-    def parse(self, phrase):
-        """ Given a phrase it will extract Intent. """
+    def parse(self, phrase: str) -> Intent:
+        """
+            Given a phrase it will extract Intent.
+        """
         doc = self.nlp(phrase)
         ticketID = self.__findTicketID(doc)
         lemma = self.__findLemma(doc)
